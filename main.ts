ca4e0f6cadbcd2ca8c16764a2ad690348ca5d25e
Aldcoder <122543592+Aldcoder@users.noreply.github.com>
2023-01-13 09:49:34 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,11 +2,16 @@ input.onButtonPressed(Button.A, function () {
     basic.showIcon(IconNames.Duck)
 })
 input.onButtonPressed(Button.AB, function () {
-    music.playMelody("C5 F E F G F A B ", 120)
+    for (let index = 0; index < 3; index++) {
+        music.playMelody("C5 F E F G F A B ", 120)
+    }
 })
 input.onButtonPressed(Button.B, function () {
     basic.showIcon(IconNames.Asleep)
 })
+function Clap () {
+    basic.showString("Hello!")
+}
 basic.showLeds(`
     # . # . #
     # . # . #
